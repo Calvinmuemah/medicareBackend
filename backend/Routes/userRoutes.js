870464b25e412b79f3eaee12e3e05bce93230b57
Willington Juma <willingtonejumamconyango@gmail.com>
2025-07-24 15:05:13 +0300

    base admin acces

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -12,13 +12,10 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-
 router.get('/user/:id', authMiddleware, getUser);
 router.put('/profile', authMiddleware, updateProfile);
 router.put('/password', authMiddleware, resetPassword);
-
 // Admin & Doctor example
 router.get('/admin/data', authMiddleware, isAdmin, (req, res) => res.send("Hello Admin"));
 router.get('/doctor/data', authMiddleware, isDoctor, (req, res) => res.send("Hello Doctor"));
-
 module.exports = router;
